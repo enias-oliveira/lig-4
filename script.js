331dfc198c3a222195d71dc1f825f49f4a27a258
Yuri Baldini <yuriabaldini@gmail.com>
2020-09-22 16:12:34 -0300

    Subindo correções

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ document.getElementById("start").addEventListener("click", start);
 function start() {
   player1 = document.getElementById("fplayer").value;
   player2 = document.getElementById("splayer").value;
+  document.getElementById("players").classList.add("Hidden");
+  document.getElementById("main").classList.remove("Hidden")
 }
 
-document.getElementById("start").addEventListener("click", restart);
+document.getElementById("restart").addEventListener("click", restart);
 
 function restart() {
   location.reload();
