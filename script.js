95e22083a00c8448d3cf14cda2af8bb036427995
Yuri Baldini <yuriabaldini@gmail.com>
2020-09-22 15:48:53 -0300

    Subindo modificações

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-document.getElementById("start").onclick = function () {
+let player1;
+let player2;
 
-
-    let player1 = document.getElementById("fplayer").value;
-    let player2 = document.getElementById("splayer").value;
-
+document.getElementById("start").addEventListener("click", start);
+function start() {
+  player1 = document.getElementById("fplayer").value;
+  player2 = document.getElementById("splayer").value;
 }
 
-document.getElementById("restart").onclick = function () {
+document.getElementById("start").addEventListener("click", restart);
 
+function restart() {
+  location.reload();
 }
