d342944faf5bfd83400f66eacbe02a7f2a162134
Yuri Baldini <yuriabaldini@gmail.com>
2020-09-22 13:20:51 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("start").onclick = function () {
+
+
+    let player1 = document.getElementById("fplayer").value;
+    let player2 = document.getElementById("splayer").value;
+
+}
+
+document.getElementById("restart").onclick = function () {
+
+}
